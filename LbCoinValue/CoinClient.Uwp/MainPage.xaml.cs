af10d2c1cb141685d9379b21176ed7f09583ad77
Lbugnion <lbugnion@outlook.com>
2017-08-30 23:41:34 +0200

    Implemented UWP client

--- LbCoinValue/CoinClient.Uwp/MainPage.xaml.cs
+++ LbCoinValue/CoinClient.Uwp/MainPage.xaml.cs
@@ -1,38 +1,12 @@
-﻿using CoinClient.Data.Model;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
 
 namespace CoinClient.Uwp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
-        }
-
-        private async void ButtonClick(object sender, RoutedEventArgs e)
-        {
-            var service = new CoinService();
-            var trend = await service.GetTrend();
-            ((Button)sender).Content = trend.CurrentValue;
+            InitializeComponent();
         }
     }
 }
