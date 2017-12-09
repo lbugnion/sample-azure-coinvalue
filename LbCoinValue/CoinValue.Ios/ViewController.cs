130a168ea4c48496d663cb76a3405ed07074dbb5
Lbugnion <lbugnion@outlook.com>
2017-12-09 21:13:38 +0100

    Added iOS project, basic UI

---
+++ LbCoinValue/CoinValue.Ios/ViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using UIKit;
+
+namespace CoinValue.Ios
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ LbCoinValue/CoinValue.Ios/ViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace CoinValue.Ios
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel BtcLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel EthLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton RefreshButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (BtcLabel != null) {
+                BtcLabel.Dispose ();
+                BtcLabel = null;
+            }
+
+            if (EthLabel != null) {
+                EthLabel.Dispose ();
+                EthLabel = null;
+            }
+
+            if (RefreshButton != null) {
+                RefreshButton.Dispose ();
+                RefreshButton = null;
+            }
+        }
+    }
+}
